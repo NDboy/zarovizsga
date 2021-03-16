bae2cb538e0035841c2c61b72b815224c8db4e5d
NDboy <endre.karsai@gmail.com>
2021-03-16 20:17:46 +0100

    1. feladat megoldás

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,4 +1,28 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
+
+    public int getCountOfDigits(String numberString) {
+        if (numberString == null) {
+            return 0;
+        }
+        Set<Integer> digits = new HashSet<>();
+        for (int i = 0; i < numberString.length(); i++) {
+            if (parseDigit(numberString.charAt(i)) != null) {
+                digits.add(parseDigit(numberString.charAt(i)));
+            }
+        }
+        return digits.size();
+    }
+
+    private Integer parseDigit(char c) {
+        if (Character.isDigit(c)) {
+            return Integer.parseInt(String.valueOf(c));
+        }
+        return null;
+    }
+
 }
