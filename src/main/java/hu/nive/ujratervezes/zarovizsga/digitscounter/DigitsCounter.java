65dd5e2f27ae946a9bf6cb6e268b5b89cbc5c69a
NDboy <endre.karsai@gmail.com>
2021-03-17 10:12:01 +0100

    1. feladat csinosítás, 1.2v

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,16 +1,12 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
-
-import java.util.stream.IntStream;
-
 public class DigitsCounter {
 
     public int getCountOfDigits(String numberString) {
         if (numberString == null) {
             return 0;
         }
-        return (int)IntStream.range(0, numberString.length())
-                .map(numberString::charAt)
+        return (int)numberString.chars()
                 .distinct()
                 .filter(Character::isDigit)
                 .count();
